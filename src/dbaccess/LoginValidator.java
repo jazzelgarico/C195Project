941c9b5d38fa14cf7396b8a7c555d1925218de3c
Jazz Elgarico <jazz.elgarico@gmail.com>
2022-02-05 15:15:28 -0600

    Added javadoc for LoginValidator

--- src/dbaccess/LoginValidator.java
+++ src/dbaccess/LoginValidator.java
@@ -1,12 +1,18 @@
 package dbaccess;
 
 import dbconnection.DBConnection;
-
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class LoginValidator {
+    /**
+     * Validates login credentials given by the user.
+     *
+     * @param username the username provided
+     * @param password the password provided
+     * @return true if login credentials are valid, false if login credentials are not valid
+     */
     public static boolean validateLogin(String username,String password) {
         boolean isLoginValid = false;
         try{
