bd29a9929b4279df3d22eec951c387d8661c8060
Jazz Elgarico <jazz.elgarico@gmail.com>
2022-02-05 18:10:13 -0600

    Added getters and setters for Appointment class

--- src/model/Appointment.java
+++ src/model/Appointment.java
@@ -29,6 +29,7 @@ public class Appointment {
 
     /**
      * Constructor for Appointment
+     *
      * @param appointmentId appointmentID to set
      * @param title title to set
      * @param description description to set
@@ -57,6 +58,203 @@ public class Appointment {
         this.userId = userId;
     }
 
+    /**
+     * Gets the appointmentId.
+     *
+     * @return the appointmentId
+     */
+    public int getAppointmentId() {
+        return appointmentId;
+    }
+
+    /**
+     * Sets the appointmentId.
+     *
+     * @param appointmentId appointmentId to set
+     */
+    public void setAppointmentId(int appointmentId) {
+        this.appointmentId = appointmentId;
+    }
+
+    /**
+     * Gets the title.
+     *
+     * @return the title
+     */
+    public String getTitle() {
+        return title;
+    }
+
+    /**
+     * Sets the title
+     *
+     * @param title title to set
+     */
+    public void setTitle(String title){
+        this.title = title;
+    }
+
+    /**
+     * Gets the description.
+     *
+     * @return the description
+     */
+    public String getDescription() {
+        return description;
+    }
+
+    /**
+     * Sets the description.
+     *
+     * @param description description to set
+     */
+    public void setDescription(String description){
+        this.description = description;
+    }
+
+    /**
+     * Gets the location.
+     *
+     * @return the location
+     */
+    public String getLocation() {
+        return location;
+    }
+
+    /**
+     * Sets the location
+     *
+     * @param location location to set
+     */
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    /**
+     * Gets the contactId
+     *
+     * @return the contactId
+     */
+    public int getContactId() {
+        return contactId;
+    }
+
+    /**
+     * Sets the contactId
+     *
+     * @param contactId contactId to set
+     */
+    public void setContactId(int contactId) {
+        this.contactId = contactId;
+    }
+
+    /**
+     * Gets the type
+     *
+     * @return the type
+     */
+    public AppointmentType getType() {
+        return type;
+    }
+
+    /**
+     * Sets the type
+     *
+     * @param type type to set
+     */
+    public void setType(AppointmentType type){
+        this.type = type;
+    }
+
+    /**
+     * Gets the appointmentDate
+     *
+     * @return the appointmentDate
+     */
+    public LocalDate getAppointmentDate() {
+       return appointmentDate; 
+    }
+
+    /**
+     * Sets the appointmentDate
+     * 
+     * @param appointmentDate appointmentDate to set
+     */
+    public void setAppointmentDate(LocalDate appointmentDate) {
+        this.appointmentDate = appointmentDate;
+    }
+
+    /**
+     * Gets the startTime
+     * 
+     * @return the startTime
+     */
+    public LocalTime getStartTime() {
+        return startTime;
+    }
+
+    /**
+     * Sets the start time
+     * 
+     * @param startTime startTime to set
+     */
+    public void setStartTime(LocalTime startTime) {
+        this.startTime = startTime;
+    }
+
+    /**
+     * Gets the endTime
+     *
+     * @return the endTime
+     */
+    public LocalTime getEndTime() {
+        return endTime;
+    }
+
+    /**
+     * Sets the end time
+     *
+     * @param endTime endTime to set
+     */
+    public void setEndTime(LocalTime endTime) {
+        this.endTime = endTime;
+    }
+
+    /**
+     * Gets the customerId
+     *
+     * @return the customerId
+     */
+    public int getCustomerId() {
+        return customerId;
+    }
+
+    /**
+     * Sets the customerId
+     *
+     * @param customerId the customerId to set
+     */
+    public void setCustomerId(int customerId) {
+        this.customerId = customerId;
+    }
+
+    /**
+     * Gets the userId
+     *
+     * @return the userId
+     */
+    public int getUserId() {
+        return userId;
+    }
+
+    /**
+     * Sets the userId
+     *
+     * @param userId userId to set
+     */
+    public void getUserId(int userId) {
+        this.userId = userId;
+    }
 
 
 }
