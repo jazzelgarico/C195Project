cc227264534062e0e73131c871762118026ab79f
Jazz Elgarico <jazz.elgarico@gmail.com>
2022-02-11 17:41:23 -0600

    Created ReportController

---
+++ src/controller/ReportController.java
@@ -0,0 +1,27 @@
+package controller;
+
+import dbaccess.DBReport;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TableColumn;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.VBox;
+
+public class ReportController implements Initializable {
+
+    @FXML
+    private VBox vbox;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+
+    }
+
+
+
+}
