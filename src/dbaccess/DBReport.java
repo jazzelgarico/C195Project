24d65752b907d909ee7c158db8bc79350308449d
Jazz Elgarico <jazz.elgarico@gmail.com>
2022-02-11 17:41:10 -0600

    Created DBReport class

---
+++ src/dbaccess/DBReport.java
@@ -0,0 +1,17 @@
+package dbaccess;
+
+import dbconnection.DBConnection;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.time.Month;
+
+
+public class DBReport {
+
+
+
+}
