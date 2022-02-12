d915066a450a79f5bfbbbd28edbc554805a75374
Jazz Elgarico <jazz.elgarico@gmail.com>
2022-02-12 00:00:56 -0600

    Added label

--- src/controller/ReportController.java
+++ src/controller/ReportController.java
@@ -3,18 +3,18 @@ package controller;
 import dbaccess.DBReport;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.TableView;
-import javafx.scene.control.TableColumn;
+import javafx.scene.control.Label;
 import java.net.URL;
 import java.util.ResourceBundle;
-
-import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.VBox;
 
 public class ReportController implements Initializable {
 
     @FXML
     private VBox vbox;
+
+    @FXML
+    private Label test;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
