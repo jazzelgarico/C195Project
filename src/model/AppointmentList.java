446964a56bcba4eb68c076b09cef87485c4b5a09
Jazz Elgarico <jazz.elgarico@gmail.com>
2022-02-11 20:09:46 -0600

    Created AppointmentList, moved list associated methods from Appointment to AppointmentList, added remove and replace methods

---
+++ src/model/AppointmentList.java
@@ -0,0 +1,35 @@
+package model;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import java.util.Optional;
+
+public class AppointmentList {
+
+    private static ObservableList<Appointment> list = FXCollections.observableArrayList();
+
+    public static void add(Appointment appointment) {
+        list.add(appointment);
+    }
+
+    public static void remove(Appointment appointment) {
+        list.remove(appointment);
+    }
+
+    public static void replace(Appointment appointment){
+        Optional<Appointment> oldAppointment = list.stream()
+                .filter(a -> a.getCustomerId() == appointment.getCustomerId())
+                .findAny();
+        if (oldAppointment.isEmpty()) {
+        } else {
+            System.out.println(oldAppointment.get());
+            System.out.println(list.indexOf(oldAppointment.get()));
+            list.set(list.indexOf(oldAppointment.get()),appointment);
+        }
+    }
+
+    public static ObservableList<Appointment> get(){
+        return list;
+    }
+
+}
