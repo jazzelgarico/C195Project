7b429c49be55f1419321d62478f8c3abd20270cf
Jazz Elgarico <jazz.elgarico@gmail.com>
2022-02-13 14:47:49 -0600

    Added setters and overrode equals and hashCode

--- src/model/MonthTypeCount.java
+++ src/model/MonthTypeCount.java
@@ -1,6 +1,7 @@
 package model;
 
 import java.time.Month;
+import java.util.Objects;
 
 public class MonthTypeCount {
     private String type;
@@ -23,5 +24,30 @@ public class MonthTypeCount {
 
     public int getCount() {
         return count;
+    }
+
+    public void setType(String type){
+        this.type = type;
+    }
+
+    public void setMonth(Month month){
+        this.month = month;
+    }
+
+    public void setCount(int count){
+        this.count = count;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        MonthTypeCount that = (MonthTypeCount) o;
+        return count == that.count && type.equals(that.type) && month == that.month;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(type, month, count);
     }
 }
