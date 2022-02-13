a6c0042fccc46391f0ab57ca26cef5f38119bf1f
Jazz Elgarico <jazz.elgarico@gmail.com>
2022-02-13 14:48:52 -0600

    Added additional reports to report list combo, added onActionComboReport

--- src/controller/ReportController.java
+++ src/controller/ReportController.java
@@ -1,12 +1,16 @@
 package controller;
 
-import dbaccess.DBReport;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.scene.layout.VBox;
+import view.MonthTypeView;
 
 public class ReportController implements Initializable {
 
@@ -16,12 +20,26 @@ public class ReportController implements Initializable {
     @FXML
     private Label test;
 
-    @Override
-    public void initialize(URL url, ResourceBundle resourceBundle) {
+    @FXML
+    ComboBox comboReport;
 
+    @FXML
+    void onActionComboReport(ActionEvent event){
+        if (comboReport.getSelectionModel().getSelectedItem() == "MonthTypeReport") {
+            vbox.getChildren().add(MonthTypeView.get());
+        } else if (comboReport.getSelectionModel().getSelectedItem() == "ContactSchedule") {
 
+        } else if (comboReport.getSelectionModel().getSelectedItem() == "Additional") {
+
+        }
     }
 
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        ObservableList<String> reportList = FXCollections.observableArrayList(
+                "MonthTypeReport","ContactSchedule","Additional");
+        comboReport.setItems(reportList);
 
+    }
 
 }
