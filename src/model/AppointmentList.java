1086fc9fecfc06a4a360cb0f1f35f60eaeda12ce
Jazz Elgarico <jazz.elgarico@gmail.com>
2022-02-18 09:20:01 -0600

    Moved checkConflict to AppointmentList

--- src/model/AppointmentList.java
+++ src/model/AppointmentList.java
@@ -2,6 +2,7 @@ package model;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import java.time.LocalDateTime;
 import java.util.Optional;
 
 public class AppointmentList {
@@ -34,4 +35,37 @@ public class AppointmentList {
         return list;
     }
 
+    /**
+     * Checks for any appointment time conflicts.
+     * <p>
+     * An appointment conflict is defined to be when (1) an appointment's start time is equal to or greater than an
+     * existing appointment's start time but less than that existing appointment's end time or (2) an appointment's
+     * end time is greater than an existing appointment's start time and the appointment's start time is before the
+     * existing appointment's end time. Appointment conflicts can happen with a new appointment or an edited appointment
+     * with new start and end times, but an edited appointment cannot be in conflict with itself.
+     *
+     * @param appointment the appointment to check for conflict
+     * @return true if there is conflict, false if there is no conflict
+     */
+    public static boolean checkConflict(Appointment appointment) {
+        int customerId = appointment.getCustomerId();
+        int appointmentId = appointment.getAppointmentId();
+        LocalDateTime start = appointment.getStartTime();
+        LocalDateTime end = appointment.getEndTime();
+
+        Optional<Appointment> startConflict = get().stream()
+                .filter(a -> a.getCustomerId() == customerId)
+                .filter(a -> a.getAppointmentId() != appointmentId)
+                .filter(a -> (start.isAfter(a.getStartTime()) || start.isEqual(a.getStartTime()))
+                        && start.isBefore(a.getEndTime()))
+                .findAny();
+
+        Optional<Appointment> endConflict = get().stream()
+                .filter(a -> a.getCustomerId() == customerId)
+                .filter(a -> a.getAppointmentId() != appointmentId)
+                .filter(a -> end.isAfter(a.getStartTime()) && start.isBefore(a.getEndTime()))
+                .findAny();
+
+        return startConflict.isPresent() || endConflict.isPresent();
+    }
 }
