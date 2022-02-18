863191dee210f653ff512bc9284684f33720ebe5
Jazz Elgarico <jazz.elgarico@gmail.com>
2022-02-18 15:49:28 -0600

    Added Javadoc, removed contactHoursSet, updated add and remove methods, added edit method to ContactHoursReport

--- src/model/ContactHoursReport.java
+++ src/model/ContactHoursReport.java
@@ -2,62 +2,89 @@ package model;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
-
 import java.time.Duration;
 import java.time.Month;
-import java.util.HashSet;
 import java.util.Optional;
-import java.util.Set;
 
+/**
+ * Models the contact-hours report which lists the number of hours each contact has scheduled for each month.
+ */
 public class ContactHoursReport {
     private static ObservableList<ContactHours> contactHoursReport = FXCollections.observableArrayList();
-    private static Set<ContactHours> contactHoursSet = new HashSet<>();
 
     public static ObservableList<ContactHours> get() {
         return contactHoursReport;
     }
 
+    /**
+     * Updates contactHoursReport with the addition of an appointment. If a ContactHours already exists with matching
+     * contact and month in contactHoursReport, adds the new appointment's duration to the existing ContactHours'
+     * duration. If a ContactHours with matching contact and month cannot be found, adds a new ContactHours using the
+     * appointment's contact, month, and duration.
+     *
+     * @param appointment the appointment to add to contactHoursReport
+     */
     public static void add(Appointment appointment) {
         Contact contact = ContactList.getContactFromId(appointment.getContactId());
         Month month = appointment.getStartTime().getMonth();
         Duration duration = Duration.between(appointment.getStartTime(),appointment.getEndTime());
 
         ContactHours chList = new ContactHours(contact, month, duration);
-        ContactHours chSet = new ContactHours(contact, month, Duration.ofHours(0));
-        if (contactHoursSet.contains(chSet)) {
-            Optional<ContactHours> chMatch = contactHoursReport.stream()
-                    .filter(ch -> ch.getContact() == contact)
-                    .filter(ch -> ch.getMonth() == month)
-                    .findFirst();
 
+        Optional<ContactHours> chMatch = contactHoursReport.stream()
+                .filter(ch -> ch.getContact() == contact)
+                .filter(ch -> ch.getMonth() == month)
+                .findFirst();
+
+        if (chMatch.isPresent()) {
             int indexCh = contactHoursReport.indexOf(chMatch.get());
             Duration chHours = contactHoursReport.get(indexCh).getTotalHours();
             contactHoursReport.get(indexCh).setTotalHours(chHours.plus(duration));
         } else {
-            contactHoursSet.add(chSet);
             contactHoursReport.add(chList);
         }
+
     }
 
+    /**
+     * Updates the contactHoursReport with the addition of the updated appointment and the removal of the original
+     * appointment.
+     *
+     * @param original the original appointment to be edited
+     * @param updated the updated appointment to replace the original
+     */
+    public static void edit(Appointment original,Appointment updated) {
+       add(updated);
+       remove(original);
+    }
+
+    /**
+     * Updates contactHoursReport with the deletion of an appointment. If the ContactHours with matching contact and
+     * month has the same duration as the appointment's duration, the ContactHours is removed from the ContactHours
+     * report. If the ContactHours with matching contact and month does not have the same duration as the
+     * appointment's duration, the appointment's duration is subtracted from the ContactHours' duration.
+     *
+     * @param appointment the appointment to remove from contactHoursReport
+     */
     public static void remove(Appointment appointment) {
         Contact contact = ContactList.getContactFromId(appointment.getContactId());
         Month month = appointment.getStartTime().getMonth();
         Duration duration = Duration.between(appointment.getStartTime(),appointment.getEndTime());
         ContactHours contactHours = new ContactHours(contact, month, duration);
+
         Optional<ContactHours> chMatch = contactHoursReport.stream()
                 .filter(ch -> ch.getContact() == contact)
                 .filter(ch -> ch.getMonth() == month)
                 .findFirst();
 
         int indexCh = contactHoursReport.indexOf(chMatch.get());
-        Duration durationList = contactHoursReport.get(indexCh).getTotalHours();
-        if (durationList == duration) {
+        Duration durationCh = contactHoursReport.get(indexCh).getTotalHours();
+
+        if (durationCh == duration) {
             contactHoursReport.remove(indexCh);
-            contactHoursSet.remove(contactHours);
         } else {
-            contactHoursReport.get(indexCh).setTotalHours(durationList.minus(duration));
+            contactHoursReport.get(indexCh).setTotalHours(durationCh.minus(duration));
         }
     }
-
 
 }
