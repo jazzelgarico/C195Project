0eb751042b2b4989b74db2513ea218f3b81acb13
Jazz Elgarico <jazz.elgarico@gmail.com>
2022-02-18 09:30:15 -0600

    Added getters and setters, added Javadoc

--- src/model/FirstLevelDivision.java
+++ src/model/FirstLevelDivision.java
@@ -1,5 +1,8 @@
 package model;
 
+/**
+ * Models First Level Divisions
+ */
 public class FirstLevelDivision {
     private int divisionId;
     private String division;
@@ -27,15 +30,68 @@ public class FirstLevelDivision {
         this.countryId = countryId;
     }
 
+    /**
+     * Gets the divisionId
+     *
+     * @return the divisionId to get
+     */
     public int getDivisionId() {
         return divisionId;
     }
 
+    /**
+     * Sets the divisionId
+     *
+     * @param divisionId the divisionId to set
+     */
+    public void setDivisionId(int divisionId) {
+        this.divisionId = divisionId;
+    }
+
+    /**
+     * Gets the division name
+     *
+     * @return the division name to get
+     */
+    public String getDivision() {
+        return division;
+    }
+
+    /**
+     * Sets the division name
+     *
+     * @param division the division name to set
+     */
+    public void setDivision(String division) {
+        this.division = division;
+    }
+
+    /**
+     * Gets the countryId
+     *
+     * @return the countryId to get
+     */
     public int getCountryId() {
         return countryId;
     }
+
+    /**
+     * Sets the countryId
+     *
+     * @param countryId the countryId to set
+     */
+    public void setCountryId(int countryId) {
+        this.countryId = countryId;
+    }
+
+    /**
+     * Returns the division name.
+     *
+     * @return
+     */
     @Override
     public String toString() {
         return division;
     }
+
 }
