580bdafad987d89ab2ba142d5666d689aa4b2c05
Jazz Elgarico <jazz.elgarico@gmail.com>
2022-02-18 15:37:12 -0600

    Added Javadoc, removed mtcReportSet, updated add and remove methods, added edit method to MonthTypeCount

--- src/model/MonthTypeReport.java
+++ src/model/MonthTypeReport.java
@@ -2,40 +2,79 @@ package model;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import java.time.Month;
+import java.util.Optional;
 
-import java.util.HashSet;
-import java.util.Set;
-
+/**
+ * Models the month-type report which lists the total number of appointments by type and month
+ */
 public class MonthTypeReport {
 
     private static ObservableList<MonthTypeCount> mtcReport = FXCollections.observableArrayList();
-    private static Set<MonthTypeCount> mtcReportSet = new HashSet<>();
 
+    /**
+     * Gets the mtcReport.
+     *
+     * @return the list of MonthTypeCount to get
+     */
     public static ObservableList<MonthTypeCount> get() {
         return mtcReport;
     }
 
+    /**
+     * Updates mtcReport with the addition of an appointment. If the month-type is already in mtcReport, increases the
+     * count by one. If the month-type is not already in mtcReport, adds it to mtcReport with an initial count of one.
+     *
+     * @param appointment the appointment to add to mtcReport
+     */
     public static void add(Appointment appointment) {
-        MonthTypeCount mtc = new MonthTypeCount(appointment.getType(),appointment.getStartTime().getMonth(),1);
-        if (mtcReportSet.contains(mtc)) {
-            int indexMtc = mtcReport.indexOf(mtc);
+        String type = appointment.getType();
+        Month month = appointment.getStartTime().getMonth();
+        MonthTypeCount initialMtc = new MonthTypeCount(type,month,1);
+        Optional<MonthTypeCount> mtcList = mtcReport.stream()
+                .filter(mtc -> mtc.getType().equals(type))
+                .filter(mtc -> mtc.getMonth() == month)
+                .findAny();
+        if (mtcList.isPresent()) {
+            int indexMtc = mtcReport.indexOf(mtcList.get());
             int countMtc = mtcReport.get(indexMtc).getCount();
-            mtcReport.get(indexMtc).setCount(countMtc+1);
+            mtcReport.get(indexMtc).setCount(countMtc + 1);
         } else {
-            mtcReportSet.add(mtc);
-            mtcReport.add(mtc);
+            mtcReport.add(initialMtc);
         }
     }
 
+    /**
+     * Adds the updated appointment to mtcReport and removes the original appointment from mtcReport.
+     *
+     * @param original the appointment to edit
+     * @param updated the new appointment to replace the original
+     */
+    public static void edit(Appointment original,Appointment updated) {
+        add(updated);
+        remove(original);
+    }
+
+    /**
+     * Updates the mtcReport with the removal of an appointment.If the month-type count is one, removes the month-type
+     * from mtcReport. If the month-type count is not one, reduces the month-type count by one.
+     *
+     * @param appointment
+     */
     public static void remove(Appointment appointment) {
-        MonthTypeCount mtc = new MonthTypeCount(appointment.getType(),appointment.getStartTime().getMonth(),1);
-        int indexMtc = mtcReport.indexOf(mtc);
+        String type = appointment.getType();
+        Month month = appointment.getStartTime().getMonth();
+        MonthTypeCount setMtc = new MonthTypeCount(type,month,1);
+        Optional<MonthTypeCount> mtcList = mtcReport.stream()
+                .filter(mtc -> mtc.getType().equals(type))
+                .filter(mtc -> mtc.getMonth() == month)
+                .findAny();
+        int indexMtc = mtcReport.indexOf(mtcList.get());
         int countMtc = mtcReport.get(indexMtc).getCount();
         if (countMtc == 1) {
             mtcReport.remove(indexMtc);
-            mtcReportSet.remove(mtc);
         } else {
-            mtcReport.get(indexMtc).setCount(countMtc-1);
+            mtcReport.get(indexMtc).setCount(countMtc - 1);
         }
     }
 
