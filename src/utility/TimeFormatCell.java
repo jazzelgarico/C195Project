f4d6946aa0ddc3477663dec16f6b34576442f31f
Jazz Elgarico <jazz.elgarico@gmail.com>
2022-02-19 11:18:12 -0600

    Updated Javadoc

--- src/utility/TimeFormatCell.java
+++ src/utility/TimeFormatCell.java
@@ -4,9 +4,22 @@ import javafx.scene.control.ListCell;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
+/**
+ * A custom format for ListCell<LocalDateTime>
+ */
 public class TimeFormatCell extends ListCell<LocalDateTime>{
 
+    /**
+     * No-arg constructor for TimeFormatCell
+     */
     public TimeFormatCell() {}
+
+    /**
+     * Override of updateItem: displays LocalDateTime in "h:mma" format if not empty or null.
+     *
+     * @param time the LocalDateTime to update
+     * @param empty true if empty, false if not empty
+     */
     @Override
     protected void updateItem(LocalDateTime time, boolean empty) {
         super.updateItem(time, empty);
