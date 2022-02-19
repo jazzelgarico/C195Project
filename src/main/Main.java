b53735201c4cd9fd538dad594bdd3f5628798972
Jazz Elgarico <jazz.elgarico@gmail.com>
2022-02-19 10:39:13 -0600

    Added javadoc

--- src/main/Main.java
+++ src/main/Main.java
@@ -9,17 +9,26 @@ import java.io.IOException;
 import java.util.Locale;
 import java.util.ResourceBundle;
 
-
+/**
+ * Starts the application
+ */
 public class Main extends Application {
+    /**
+     * Starts the application with login view. Starts the title of the window by using the appropriate greting from
+     * the resource bundle.
+     */
     @Override
-    public void start(Stage stage) throws IOException {
+    public void start(Stage stage) {
+        try {
         ResourceBundle rb = ResourceBundle.getBundle("Lang",Locale.getDefault());
         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("../view/login-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle(rb.getString("Welcome")+"!");
         stage.setScene(scene);
         stage.show();
-
+        } catch (IOException e){
+            e.printStackTrace();
+        }
     }
 
     public static void main(String[] args) {
