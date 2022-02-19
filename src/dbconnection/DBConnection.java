c7ed8b53e380bbb22240a471ee373f3d2b40e63a
Jazz Elgarico <jazz.elgarico@gmail.com>
2022-02-19 10:34:22 -0600

    Added Javadoc to DBConnection

--- src/dbconnection/DBConnection.java
+++ src/dbconnection/DBConnection.java
@@ -4,35 +4,48 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
+/**
+ * Connects to database
+ */
 public class DBConnection {
     private static final String protocol = "jdbc";
     private static final String vendor = ":mysql:";
     private static final String location = "//localhost/";
     private static final String databaseName = "client_schedule";
-    private static final String jdbcUrl = protocol + vendor + location + databaseName + "?connectionTimeZone = SERVER"; // LOCAL
-    private static final String userName = ""; // Username
-    private static String password = ""; // Password
-    public static Connection conn = null;  // Connection Interface
+    private static final String jdbcUrl = protocol + vendor + location + databaseName + "?connectionTimeZone = SERVER";
+    private static final String userName = "";
+    private static String password = "";
+    public static Connection conn = null;
 
+    /**
+     * Opens the connection to the database
+     */
     public static void openConnection()
     {
         try {
             conn = DriverManager.getConnection(jdbcUrl, userName, password); // Reference Connection object
-            System.out.println("Connection successful!");
         } catch(SQLException e) {
             e.printStackTrace();
         }
     }
 
+    /**
+     * Gets the connection to the database
+     * @return the connection to the database
+     */
     public static Connection getConnection(){
         return conn;
     }
 
+    /**
+     * Closes the connection to the database
+     */
     public static void closeConnection() {
         try {
             conn.close();
-            System.out.println("Connection closed!");
-        } catch(Exception e) {}
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
     }
 
 }
